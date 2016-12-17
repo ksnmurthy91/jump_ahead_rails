0e6bf0070ba8e05a0b0d826070e28f6d09054d80
ksnmurthy <ksnmurthy4564@gmail.com>
2016-12-17 01:51:05 -0500

    partials, destroy, edit, show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,35 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
+    def edit
+
+       @article = Article.find(params[:id])
+
+    end
+
+    def update
+
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+      flash[:notice] = "article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+    end
+    def index
+
+      @articles = Article.all
+    end
+
+     def destroy
+
+       @article = Article.find(params[:id])
+       @article.destroy
+       flash[:notice] = "Article was successfully deleted"
+       redirect_to articles_path
+
+     end
   private
       def article_params
 
