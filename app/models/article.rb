bd709a600169386638d8ff805f839cf021ebcab1
ksnmurthy <ksnmurthy4564@gmail.com>
2016-12-17 00:00:33 -0500

    Tried edit, update and destroy from models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
 
-
+#add validations to enforce constraints
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
 end
 
